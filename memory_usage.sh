5e0d74b6ad0e548564c6dac2e62fe151120bad59
matyalatte <matyalatte@gmail.com>
2024-08-17 10:28:49 +0900

    measure memory usage in benchmark.yml

---
+++ memory_usage.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Script to measure memory usage with cpplint
+#
+# Examples
+#  sh memory_usage.sh "python3 cpplint.py" .
+#  sh memory_usage.sh "./build/cpplint-cpp" ../googletest-1.4.0
+
+echo Measuring memory usage...
+kib="$(/usr/bin/time -f "%M" sh -c "$1 --recursive --quiet --counting=detailed $2 >/dev/null 2>&1 || exit 0" 2>&1)"
+mib=$(echo "scale=2; $kib / 1024" | bc)
+echo Maximum memory usage: ${mib} MiB
