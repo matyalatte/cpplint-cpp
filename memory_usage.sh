e57d1d151ce463800ef35801245a6c42c27b32d2
matyalatte <matyalatte@gmail.com>
2024-09-07 15:52:30 +0900

    improve benchmark.yml
    - Run benchmark.yml when pushing PRs.
    - Extend measuring time from 30 sec to 1 min.
    - Show executable path in memory_usage.sh.
    - Run cpplint-cpp first when measuring memory usage.

--- memory_usage.sh
+++ memory_usage.sh
@@ -5,7 +5,7 @@
 #  sh memory_usage.sh "python3 cpplint.py" .
 #  sh memory_usage.sh "./build/cpplint-cpp" ../googletest-1.4.0
 
-echo Measuring memory usage...
+echo Measuring memory usage: $1
 kib="$(/usr/bin/time -f "%M" sh -c "$1 --recursive --quiet --counting=detailed $2 >/dev/null 2>&1 || exit 0" 2>&1)"
 mib=$(echo "scale=2; $kib / 1024" | bc)
 echo Maximum memory usage: ${mib} MiB
